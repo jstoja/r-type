84855d4589714d0f68c85faeef8b9ebe8367213d
Julien Bordellier <julienbordellier@gmail.com>
2012-12-18 17:53:33 +0100

    new main for test portability

--- Client/src/main.cpp
+++ Client/src/main.cpp
@@ -1,3 +1,40 @@
-int main(int ac, char **av) {
+#include <iostream>
+#include "Threading.h"
+
+#include <unistd.h>
+
+Threading::Mutex g_mutex;
+Threading::Atomic<int> g_atomic;
+
+class Test
+{
+public:
+	Test() {}
+	~Test() {}
+
+	void	operator()()
+	{
+		Threading::MutexLocker	locker(&g_mutex);
+		std::cout << "PLOP" << std::endl;
+		sleep(2);
+		g_atomic = 42;
+		std::cout << "FINISHING" << std::endl;
+	}
+};
+
+int	main(int ac, char **av)
+{
+	Threading::Thread<Test> tmp(new Test);
+	g_atomic = 2;
+
+	Threading::MutexLocker	locker(&g_mutex);
+	tmp.run();
+	getchar();
+	locker.unlock();
+	std::cout << "Before join" << g_atomic << std::endl;
+	locker.relock();
+	tmp.join();
+	std::cout << "After join" << g_atomic << std::endl;
+	sleep(2);
 	return (0);
 }
