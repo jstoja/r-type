7002ab5ca55b8526521e2f34a7f3a70ca1682bf0
Gael du Plessix <gael.duplessix@gmail.com>
2012-12-19 14:18:19 +0100

    Added resources. Updated CMakeLists.txt so it generates a Bin/ directory with binaries and resources

--- configure.sh
+++ configure.sh
@@ -7,6 +7,6 @@ cmake .. $@
 cd ..
 echo "Generating Makefile and clean.sh"
 echo -e "all:\n\tmake -C build" > Makefile
-echo -e "#!/bin/bash\n\nrm -Rf build\nrm -f Makefile clean.sh" > clean.sh
+echo -e "#!/bin/bash\n\nrm -Rf build Bin\nrm -f Makefile clean.sh" > clean.sh
 chmod +x clean.sh
 echo "Done"
