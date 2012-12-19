5817313e425fda2eb576957ffe9e60af8b11509c
Franck Lavisse <francklavisse@MacBook-Pro-de-Franck.local>
2012-12-20 00:09:35 +0100

    test SoundManager

--- Tests/SoundManager.cpp
+++ Tests/SoundManager.cpp
@@ -6,15 +6,18 @@
 //
 //
 
-#include "SoundManager.h"
+#include "Debug.hpp"
+#include "../Client/src/Sound/SoundManager.h"
 
-int	main() {
+int	main(int argc, char *argv[]) {
     Sound::SoundManager	manager;
     
     manager.load("marseille.wav");
     manager.load("highpass.wav");
     manager.play("marseille.wav");
     manager.play("highpass.wav");
+    std::cout << manager.isPlaying("highpass.wav") << ' ' << manager.isPlaying("marseille.wav") << std::endl;
     sleep(2);
+    std::cout << manager.isPlaying("highpass.wav") << ' ' << manager.isPlaying("marseille.wav") << std::endl;
     return (0);
 }
