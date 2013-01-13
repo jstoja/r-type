eca437bc4600b3dfd32e9d8e774e55fb225cb354
Franck Lavisse <francklavisse@MacBook-Pro-de-Franck.local>
2013-01-13 16:44:49 +0100

    merge

--- Client/src/Widgets/TextEdit.cpp
+++ Client/src/Widgets/TextEdit.cpp
@@ -39,7 +39,7 @@ std::string const&  Widget::TextEdit::getText() const {
 }
 
 void    Widget::TextEdit::setText(std::string const& text) {
-    _label.setText(text);o
+    _label.setText(text);
     update();
     _eventListener = new Event::Listener(
                                          Event::PointerPushed
