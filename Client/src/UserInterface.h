be9bb6005343457d1f112f43bc32dcf5e80cec89
Gael du Plessix <gael.duplessix@gmail.com>
2013-01-14 12:36:34 +0100

    Added User Interface class

---
+++ Client/src/UserInterface.h
@@ -0,0 +1,22 @@
+//
+//  UserInterface.h
+//  R-Type
+//
+//  Created by Gael du Plessix on 14/01/13.
+//  Copyright (c) 2013 EPITECH. All rights reserved.
+//
+
+#ifndef __R_Type__UserInterface__
+# define __R_Type__UserInterface__
+
+class UserInterface {
+public:
+    
+    UserInterface(void);
+    ~UserInterface(void);
+    
+private:
+    
+};
+
+#endif /* defined(__R_Type__UserInterface__) */
