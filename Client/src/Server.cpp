1d443591a4fdc64462e41c7009f6c50e8f0e2435
Gael du Plessix <gael.duplessix@gmail.com>
2013-01-18 10:49:26 +0100

    Added added close event to user interface

---
+++ Client/src/Server.cpp
@@ -0,0 +1,9 @@
+//
+//  Server.cpp
+//  R-Type
+//
+//  Created by Gael du Plessix on 18/01/13.
+//
+//
+
+#include "Server.h"
---
+++ Client/src/Server.h
@@ -0,0 +1,24 @@
+//
+//  Server.h
+//  R-Type
+//
+//  Created by Gael du Plessix on 18/01/13.
+//
+//
+
+#ifndef __R_Type__Server__
+# define __R_Type__Server__
+
+class Server {
+public:
+    
+    Server(void);
+    ~Server(void);
+    
+
+private:
+    
+    
+};
+
+#endif /* defined(__R_Type__Server__) */
