c6fbb12b67da2146eae6e2d19c3cab1bdde5adf3
Julien Bordellier <julienbordellier@gmail.com>
2013-01-19 17:30:26 +0100

    Added a getEllapsedTime method in IGame+Game and created a new plugin Monster

---
+++ Server/Plugins/Monster/src/Monster.cpp
@@ -0,0 +1,96 @@
+//
+// Monster.cpp for R-Type in /home/bordel_/R-Type
+//
+// Made by Julien Bordellier
+// Login   <bordel@epitech.net>
+//
+// Started on  Sam. janv. 19 13:48:32 2013 Julien Bordellier
+//
+
+#include <sstream>
+#include "Monster.h"
+
+Monster::Monster(std::string const& pluginName)
+: _name(pluginName), _game(NULL) {
+    
+}
+
+Monster::~Monster() {
+}
+
+bool	Monster::init(IGame* game, ByteArray const& params, float32 xStart) {
+ 	if ((_game = game) == NULL)
+		return false;
+    std::stringstream	data(std::stringstream::binary);
+	data.write(params.getData(), params.getSize());
+
+    Vec3 pos;
+    Vec2 size;
+    float32 rotation;
+    char *spriteName;
+    uint32 spriteSize;
+    char frameIndex;
+    
+    _xStart = xStart;
+    
+    data.read(reinterpret_cast<char*>(&spriteSize), sizeof(spriteSize));
+    
+    spriteName = new char[spriteSize];
+	data.read(spriteName, spriteSize*sizeof(*spriteName));
+	std::string name = std::string(spriteName, spriteSize);
+	delete []spriteName;
+
+    data.read(reinterpret_cast<char*>(&frameIndex), sizeof(frameIndex));
+    
+    ISprite	*sprite = _game->getLevelSprite(name);
+	if (sprite == NULL)
+		return false;
+	if ((_graphicElement = game->createGraphicElement()) == NULL)
+		return false;
+	if ((_physicElement = game->createPhysicElement()) == NULL)
+		return false;
+    
+    data.read(reinterpret_cast<char*>(&pos), sizeof(pos));
+    data.read(reinterpret_cast<char*>(&size), sizeof(size));
+    data.read(reinterpret_cast<char*>(&rotation), sizeof(rotation));
+
+    
+    data.read(reinterpret_cast<char*>(&_speed), sizeof(_speed));
+
+	_graphicElement->setPosition(pos);
+	_graphicElement->setSize(size);
+	_graphicElement->setRotation(rotation);
+	_graphicElement->setSprite(sprite);
+	_graphicElement->setSpriteFrameIndex(frameIndex);
+	_graphicElement->setType(IGraphicElement::Static);
+	game->addGraphicElement(_graphicElement);
+	_physicElement->setPosition(pos);
+	_physicElement->setSize(size);
+	_physicElement->setRotation(rotation);
+	_physicElement->setType(IPhysicElement::Static);
+	game->addPhysicElement(_physicElement);
+	return true;
+}
+
+void	Monster::update() {
+    Vec3 newPosition = _graphicElement->getPosition();
+    
+    // NOT SO REAL
+    newPosition.x += _speed*_game->getEllapsedTime()/1000;
+    //
+    
+    if (newPosition.x > _game->getViewPort()->getPosition()) {
+        _xStart = -1;
+    } else {
+        _graphicElement->setPosition(newPosition);
+        _physicElement->setPosition(newPosition);
+    }
+}
+
+float32	Monster::getXStart() const {
+    return (_xStart);
+}
+
+std::string const&	Monster::getName() const {
+    return (_name);
+}
